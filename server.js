074cce7063df10502c3c6ed03fe4ae307017bb46
ruhithpasha <ruhithpasha813@gmail.com>
2025-08-02 13:03:49 +0530

    cors error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite default port
+  origin: '*', // Allow all origins
   credentials: true
 }));
 app.use(express.json());
